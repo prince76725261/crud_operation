24340f168d189c46fee19c6da20f07338c44c2f1
Prince Kumar Singh <prince.singh@tataaig.com>
2024-10-24 02:10:05 +0530

    fix:config file changes3 and index file moved to public

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: 'src',
   build: {
     rollupOptions: {
       external: ['react-icons/fa', 'react-icons/ri','uuid'],
